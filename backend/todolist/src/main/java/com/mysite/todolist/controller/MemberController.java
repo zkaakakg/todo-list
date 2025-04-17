a91e93da3f885d57a565c80d3c99509fd10d3c63
zkaakakag <cscom222@naver.com>
2025-04-17 09:57:42 +0900

    feat: 회원가입 중복 예외 처리 로직 추가

--- backend/todolist/src/main/java/com/mysite/todolist/controller/MemberController.java
+++ backend/todolist/src/main/java/com/mysite/todolist/controller/MemberController.java
@@ -1,13 +1,11 @@
 package com.mysite.todolist.controller;
 
 import com.mysite.todolist.dto.MemberDto;
+import com.mysite.todolist.exception.DuplicateResourceException;
 import com.mysite.todolist.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,14 +15,17 @@ public class MemberController {
 
     @PostMapping("/signup")
     @ResponseBody
-    public String signup(@RequestBody MemberDto dto){
-        try{
+    public String signup(@RequestBody MemberDto dto) {
+        try {
             memberService.create(dto);
-        }catch (Exception e){
+        } catch (DuplicateResourceException e) {
+            return e.getMessage();
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
             return "회원가입 실패";
         }
         return "회원가입 성공";
     }
+
 }
