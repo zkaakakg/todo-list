b916b5c5a9ee0978d8a1697c63ac420716333475
zkaakakag <cscom222@naver.com>
2025-04-17 10:00:01 +0900

    feat: 예외 응답 객체 ErrorResponse 정의

---
+++ backend/todolist/src/main/java/com/mysite/todolist/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.mysite.todolist.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
