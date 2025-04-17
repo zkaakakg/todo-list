f166b7e4f9d46d58fafc3ff9b9054fae75078a8b
zkaakakag <cscom222@naver.com>
2025-04-17 10:02:03 +0900

    feat: 사용자 중복 체크 및 조회 메서드 추가 (username, email)

--- backend/todolist/src/main/java/com/mysite/todolist/repository/MemberRepository.java
+++ backend/todolist/src/main/java/com/mysite/todolist/repository/MemberRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-//    Optional<Member> findByUsername(String username);
+    Optional<Member> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
 
