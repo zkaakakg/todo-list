352adebab1b81ac6ec97d0728c31b8bf356f9b0d
zkaakakag <cscom222@naver.com>
2025-04-17 10:03:43 +0900

    feat: 회원가입 시 중복 검사 및 비밀번호 인코딩 처리 추가

--- backend/todolist/src/main/java/com/mysite/todolist/service/MemberService.java
+++ backend/todolist/src/main/java/com/mysite/todolist/service/MemberService.java
@@ -1,16 +1,28 @@
 package com.mysite.todolist.service;
 
 import com.mysite.todolist.dto.MemberDto;
+import com.mysite.todolist.entity.Member;
+import com.mysite.todolist.exception.DuplicateResourceException;
 import com.mysite.todolist.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void create(MemberDto memberDto){
-        this.memberRepository.save(memberDto.toEntity());
+    public void create(MemberDto memberDto) {
+        if (memberRepository.existsByUsername(memberDto.getUsername()) || memberRepository.existsByEmail(memberDto.getEmail())) {
+            throw new DuplicateResourceException("이미 존재하는 회원입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(memberDto.getPassword());
+
+        Member member = memberDto.toEntity(encodedPassword);
+
+        this.memberRepository.save(member);
     }
 }
