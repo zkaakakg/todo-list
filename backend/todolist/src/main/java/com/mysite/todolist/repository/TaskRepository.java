482bcabc9f5353928ffdff51558f28a645e76d4e
zkaakakag <cscom222@naver.com>
2025-04-21 17:27:53 +0900

    feat: TaskRepository에 사용자 기반 조회 메서드 추가

--- backend/todolist/src/main/java/com/mysite/todolist/repository/TaskRepository.java
+++ backend/todolist/src/main/java/com/mysite/todolist/repository/TaskRepository.java
@@ -2,6 +2,13 @@ package com.mysite.todolist.repository;
 
 import com.mysite.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByMemberId(Long member);
+    List<Task> findAllByMemberId(Long member);
 }
