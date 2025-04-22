3aa449d1f21a001a33c9e44fef14221c9a28b924
zkaakakag <cscom222@naver.com>
2025-04-22 17:23:13 +0900

    feat: Task 엔티티에 toDto, update, statusUpdate 메서드 추가

--- backend/todolist/src/main/java/com/mysite/todolist/entity/Task.java
+++ backend/todolist/src/main/java/com/mysite/todolist/entity/Task.java
@@ -1,10 +1,12 @@
 package com.mysite.todolist.entity;
 
+import com.mysite.todolist.dto.TaskDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 
@@ -26,12 +28,32 @@ public class Task {
     @Column(name = "due_date")
     private LocalDate dueDate;
     @Column(name = "create_at")
+    @CreationTimestamp
     private LocalDate createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
+    public TaskDto toDto() {
+        return TaskDto.builder()
+                .id(id)
+                .title(title)
+                .status(status)
+                .dueDate(dueDate)
+                .memberId(member != null ? member.getId() : null)
+                .build();
+    }
+
+    public void update(String title, String status, LocalDate DueDate){
+        this.title = title;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public void statusUpdate(String status){
+        this.status = status;
+    }
 
 
 }
