c0729d06aa7215163023b9d46de91bb73140a87f
zkaakakag <cscom222@naver.com>
2025-04-23 09:50:31 +0900

    chore: Repository 어노테이션 추가

--- backend/todolist/src/main/java/com/mysite/todolist/repository/MemberRepository.java
+++ backend/todolist/src/main/java/com/mysite/todolist/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.mysite.todolist.repository;
 
 import com.mysite.todolist.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
     Optional<Member> findByEmail(String email);
