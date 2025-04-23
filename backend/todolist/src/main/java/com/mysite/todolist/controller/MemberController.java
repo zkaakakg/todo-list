e74f4e441d98a7bc20634fcd29c4c7fafaaef416
zkaakakag <cscom222@naver.com>
2025-04-23 09:41:37 +0900

    refactor: MemberController에서 RestController 통합 및 매핑 경로 정리

--- backend/todolist/src/main/java/com/mysite/todolist/controller/MemberController.java
+++ backend/todolist/src/main/java/com/mysite/todolist/controller/MemberController.java
@@ -8,19 +8,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    @ResponseBody
     public String signup(@RequestBody MemberDto dto) {
         try {
             memberService.create(dto);
@@ -34,7 +32,7 @@ public class MemberController {
         return "회원가입 성공";
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<?> getCurrentUser(Authentication authentication) {
         if (authentication == null || !authentication.isAuthenticated()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인되지 않았습니다.");
